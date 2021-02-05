dbad234df2d376494d6757143ceae604cd3a77c8
Ryan McNair <ryan.mcnair@gmail.com>
2021-02-04 19:18:33 -0600

    in class code along

---
+++ LinqExamples/Examples.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CollectionsAndLinq.LinqExamples
+{
+    class Examples
+    {
+        public void Run()
+        {
+
+            //new list of whole numbers
+            var numbers = new List<int> { 12, 15, 400, 1, 3208, 19, 12, 400, 300, 3209 };
+            var badNumbers = new List<int> { 666, 13, 19 };
+
+            //Where is like Array.filter, returns a new collection of IEnumerable<T>
+            //filtering data
+            var bigNumbers = numbers.Where(number => number > 27);
+
+            //Select is like Array.map, returns a new collection of IEnumerable<T>
+            //transforming data
+            var biggerNumbers = numbers.Select(number => number + 27);
+
+            //the "max" item in a collection
+            var biggestNumber = numbers.Max();
+
+            //the first item of a collection
+            var firstNumber = numbers.First();
+
+            //first matching item
+            var firstMatchingNumber = numbers.First(number => number > 12);
+
+            //contains but cooler, is there a value contained... is there a specific value contained within the collection
+            var hasReallyBigNumbers = numbers.Any(number => number > 1000000);
+
+            //Complex/Referenc Types and Linq
+            var animals = new List<Animal>
+            {//collection initializer
+                new Animal{Type = "Giraffe", HeightInInches = 204, WeightInPounds = 1800}, //object initializer
+                new Animal{Type = "Tiger", HeightInInches = 40, WeightInPounds = 500},//<-- must include comma
+                new Animal{Type = "Frog", HeightInInches = 3, WeightInPounds = 0},
+                new Animal{Type = "Gorilla", HeightInInches = 63, WeightInPounds = 3500}
+            };
+
+            //filter data
+            var animalsThatStartWithG = animals.Where(animal => animal.Type.StartsWith("G")).ToList();
+
+            //transform data
+            var animalDescriptions = animals
+                .Select(animal => $@"A {animal.Type} is {animal.HeightInInches} inches tall and {animal.WeightInPounds}lbs heavy.");
+
+            foreach (var description in animalDescriptions)
+            {
+                Console.WriteLine(description);
+            }
+
+            //group a collection by a given key (based on a function)
+            var groupAnimals = animals.GroupBy(animal => animal.Type.First());
+
+            foreach (var animalGroup in groupAnimals)
+            {
+                Console.WriteLine($"Animals that start with {animalGroup.Key}");
+
+                foreach (var animal in animalGroup)
+                {
+                    Console.WriteLine(animal.Type);
+                }
+            }
+                                                                //Key                   //Value
+            var groupAnimalNames = animals.GroupBy(animal => animal.Type.First(), animal => animal.Type);
+
+            var filteredAndTransformedAnimals = animals
+                .Where(animal => animal.HeightInInches > 20)
+                .Select(animal => animal.Type);
+
+            var firstThreeNumbersAndTheSixth = numbers.Take(3).Concat(numbers.Skip(5).Take(1));
+
+            //removes a set of numbers from a collection held within another collection
+            var onlyGoodNumbers = numbers.Except(badNumbers);
+
+            //no duplicates
+            var uniqueNumbers = numbers.Distinct();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using CollectionsAndLinq.LinqExamples;
+
+namespace CollectionsAndLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //anything with angle brackets after the type name is a generic type
+            //List<T> Generic List
+            //Pronounced List of T
+            //T is a Generic Type Parameter
+            //string in this case closes the generic type
+            var teachers = new List<string>() {"Jameka", "Dylan", "Nathan"};
+            var e13 = new List<string>();
+            e13.Add("Rob");
+            e13.Add("Ryan");
+            e13.Add("Bailey");
+            e13.Add("Wanda");
+            e13.Add("Wanda");
+            e13.Add("Wanda");
+            e13.Add("Hunder");
+
+            //e13.Add(teachers[0]);
+            //e13.Add(teachers[1]);
+            //e13.Add(teachers[2]);
+
+            //foreach (var student in e13)
+            //{
+            //    Console.WriteLine(student);
+            //}
+
+            //gets enumerator for a type
+            //foreach (var student in e13)//unsupported operation exception
+            //{
+            //    //changes to the underlying collection do not allow for continued looping
+            //    e13.Add("myself"); 
+            //}
+
+            e13.AddRange(teachers);
+
+            if (e13.Remove("Wanda"))
+            {
+                Console.WriteLine("Bye Wanda");
+            }
+            if (e13.Remove("Wanda"))
+            {
+                Console.WriteLine("Bye again Wanda");
+            }
+
+            //Dictionary<TKey, TValue>
+            //Arity - How many generic type parameters a collection takes in. Dictionary `2.
+            //Arity (`2) Takes in 2 parameters
+            //Very fast information retrieval
+            //Slower information storage
+            //Good for: infrequently updated but often read data
+            //Good for: loading information at startup or in the background and fast retrieval on demand (caching)
+
+            //the first parameter is the type for the keyk, the second is the type for the value
+            var words = new Dictionary<string, string>
+            { //collection initializer
+                {"soup", "a type of food that is good." }, //key value pair
+                {"cake", "a type of food that is good." }
+            };
+
+            words.Add("Arity", "How many generic type parameters a collection takes or passes in");
+
+            words["Arity"] = "A thing Nathan made up"; //look up an item by key using the indexer
+
+            //words.Add("Arrity", "new definition"); //argument exception
+            if(!words.TryAdd("Arrity", "new definition"))
+            {
+                words["Arity"] = "Another definition";
+            }
+
+            words.Remove("cake"); //only need to pass the key
+
+            foreach (var word in words)
+            {
+                Console.WriteLine($"{word.Key} means {word.Value}");
+            }
+
+            //destructured version of the above foreach loop
+            foreach (var (word, definition) in words)
+            {
+                Console.WriteLine($"{word} means {definition}");
+            }
+
+            var complicatedDictionary = new Dictionary<string, List<string>>();
+
+            complicatedDictionary.Add("Soup", new List<string>());
+            complicatedDictionary["Soup"].Add("This is a definition of soup");
+            complicatedDictionary.Add("Arity", new List<string> { "A definition of arity" });
+
+            foreach (var (word, definitions) in complicatedDictionary)
+            {
+                Console.WriteLine(word);
+                foreach (var definition in definitions)
+                {
+                    Console.WriteLine($"/t{definition}");
+                }
+            }
+
+            //Hashset<T>
+            //Really fast retrieval, no keys
+            //Slow information storage
+            //enforces uniqueness, but no errors
+            //Good for: looping, de-duplication
+            //Good for: when you only want at most one copy of a thing
+                    
+            var unique = new HashSet<string>(e13); //most collection constructors take in collections and convert them
+
+            unique.Add("Ryan"); //only allows for one copy to add
+            unique.Add("Ryan"); //these others will not add to "unique"
+            unique.Add("Ryan");
+            unique.Add("Ryan");
+
+            unique.Remove("Ryan");
+
+            //Queue<T>
+            //FIFO - first in first out
+            var queue = new Queue<int>();
+            queue.Enqueue(5);
+            queue.Enqueue(8);
+            queue.Enqueue(12);
+            queue.Enqueue(3);
+            queue.Enqueue(1);
+
+            while (queue.Count > 0)
+            {
+                Console.WriteLine($"currently dequeueing : {queue.Dequeue()} ");
+            }
+
+
+            //Stact<T>
+            //LIFO - last in first out
+            var stack = new Stack<int>();
+            stack.Push(5);
+            stack.Push(8);
+            stack.Push(5);
+
+            while (queue.Count > 0)
+            {
+                Console.WriteLine($"currently dequeueing : {stack.Pop()} ");
+            }
+
+            //var examples = new Examples();
+        }
+    }
+}
